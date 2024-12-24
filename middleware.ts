6f46a76bf983de307e63a976a69f90b90f0d18bc
Aydin Joshi <ajoshi1336@gmail.com>
2024-12-24 17:55:51 -0500

    Protected route redirect bug fix

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,23 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-up(.*)',
+  '/sign-in(.*)'
+])
 
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect();
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    return NextResponse.redirect(new URL('/sign-in', req.url))
   }
-});
+})
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
-};
+}
